0d6189910aec30fe36cdcb6c2e9b81abd42eb2c4
MaghodiyaVivek <123167310+MaghodiyaVivek@users.noreply.github.com>
2023-02-17 13:34:19 +0530

    Add files via upload

---
+++ Task/nested loop.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+
+{
+    int v, k;
+    for(v = 0; v < 7; v++)
+    {
+
+        for(k = 0; k <= v; k++)
+        {
+            printf("%d ", v);
+        }
+        printf("\n");
+
+    }
+}
