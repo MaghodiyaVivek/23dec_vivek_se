b9c783821df8711c1c65ed3d6551f4e3c9d7b7fb
MaghodiyaVivek <123167310+MaghodiyaVivek@users.noreply.github.com>
2023-03-14 10:52:12 +0530

    Add files via upload

---
+++ Assessment/Assessment 1.cpp
@@ -0,0 +1,48 @@
+include<stdio.h>
+void main()
+{
+	int a[2][2],b[2][2],c[2][2],i,j,k,sum=0;
+	clrscr();
+
+	Printf("Enter the value of matrix a:\n");
+	for(i=0; i<2; i++)
+	{
+		for(j=0; j<2; i++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+		
+	}
+	
+	Printf("Enter the value of matrix b:\n");
+	for(i=0; i<2; i++)
+	{
+		for(j=0; j<2; i++)
+		{
+			scanf("%d",&b[i][j]);
+		}
+		
+	}
+	for(i=0; i<2; i++)
+	{
+		for(j=0; j<2; j++)
+		{
+			sum=0;
+			for(k=0; k<2; k++)
+			{
+				sum=sum+a[i][k]*b[k][j];
+			}
+			c[i][j]=sum;
+		}
+	}
+	printf("Multiplication:\n");
+	for(i=0; i<2; i++)
+	{
+		for(j=0; j<2; j++)
+		{
+			printf("%d",c[i][j]);
+		}
+		printf(\n);
+	}
+
+}	
