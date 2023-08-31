d1586992171c87e766a8972223cc76e8afc8bdc3
MaghodiyaVivek <123167310+MaghodiyaVivek@users.noreply.github.com>
2023-08-31 11:02:33 +0530

    Add files via upload

---
+++ Assignment/Modual-3.1/Module 3.4.cpp
@@ -0,0 +1,18 @@
+// find simple interest
+//principlr,time,p,r,t,si;
+
+#include<stdio.h>
+main()
+
+{
+	float p,r,t,a;
+	printf("Enter a principal :");
+	scanf("%f",&p);
+	printf("Enter a interest :");
+	scanf("%f",&r);
+	printf("Enter a time :");
+	scanf("%f",&t);
+	a=(p*r*t)/100;
+	printf("simple  interest : %f",a);
+	
+}
